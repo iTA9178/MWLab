611fe9c4e042552ecdd56356c10a27cf7d5fac22
mingwei <ita9178@gmail.com>
2017-01-10 16:31:12 +0800

    创建MWLabDemo工程

---
+++ MWLabDeom/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MWLabDeom
+//
+//  Created by mingwei on 1/10/17.
+//  Copyright © 2017 keyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MWLabDeom/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MWLabDeom
+//
+//  Created by mingwei on 1/10/17.
+//  Copyright © 2017 keyue. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
